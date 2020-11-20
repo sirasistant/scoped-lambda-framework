42ef2d8a4ea8d093765a77d8eb16f5a8aab5cb86
Alvaro <sirasistant@gmail.com>
2020-11-20 17:32:34 +0100

    fix(wrappers): fixed alias extraction
    affects: scoped-lambda-wrappers

--- packages/wrappers/tests/lambdaWrapper.unit.test.js
+++ packages/wrappers/tests/lambdaWrapper.unit.test.js
@@ -2,14 +2,14 @@ import { Context, logger, setLogger } from 'scoped-lambda-context';
 import createTestLogger from './testLogger';
 import LambdaWrapper from '../src/lambdaWrapper';
 
-const invokedFunctionArn = 'arn:aws:lambda:region:account:function:functionName:alias';
+const invokedFunctionArn = 'arn:aws:lambda:eu-west-1:12345:function:functionName:invokedAlias';
 
 beforeAll(() => {
 	setLogger(createTestLogger());
 	logger.level = 'info';
-	process.env.ACCOUNT_ID = 'ACCOUNT_ID';
-	process.env.AWS_REGION = 'REGION';
-	process.env.STAGE = 'STAGE';
+	process.env.ACCOUNT_ID = '12345';
+	process.env.AWS_REGION = 'eu-west-1';
+	process.env.STAGE = 'STAGING';
 	process.env.AWS_LAMBDA_FUNCTION_VERSION = '42';
 });
 
@@ -28,4 +28,23 @@ it('Should have the same EXECUTION context for executions of the same lambda', a
 	expect(firstCallContexts[0]).toBe(secondCallContexts[0]);
 	// Event context should not be the same
 	expect(firstCallContexts[1]).not.toBe(secondCallContexts[1]);
+});
+
+it('Should set the context variables', async () => {
+	const event = { foo: 'bar' };
+	const awsEventContext = { invokedFunctionArn };
+	class TestLambda {
+		processEvent() {
+			expect(Context.getContextVariable('ACCOUNT_ID')).toBe('12345');
+			expect(Context.getContextVariable('REGION')).toBe('eu-west-1');
+			expect(Context.getContextVariable('STAGE')).toBe('STAGING');
+			expect(Context.getContextVariable('VERSION')).toBe('42');
+			expect(Context.getContextVariable('EVENT')).toBe(event);
+			expect(Context.getContextVariable('AWS_EVENT_CONTEXT')).toBe(awsEventContext);
+			expect(Context.getContextVariable('ALIAS')).toBe('invokedAlias');
+			expect(Context.getContextVariable('EVENT_ALIAS')).toBe('invokedAlias');
+		}
+	}
+	const wrapped = LambdaWrapper.wrapClass(TestLambda);
+	await wrapped(event, awsEventContext);
 });
